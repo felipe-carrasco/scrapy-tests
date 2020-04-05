7b7ce5764625b42e81a874cf4bad3ed945bb2dd0
Felipe Carrasco Villanueva <felipe.carrasco.villanueva@gmail.com>
2020-04-05 13:28:27 -0400

    add validation if product is on sale

--- ferretek/ferretek/spiders/ferretek_crawler.py
+++ ferretek/ferretek/spiders/ferretek_crawler.py
@@ -7,7 +7,9 @@ class FerretekCrawlerSpider(scrapy.Spider):
     name = 'ferretek_crawler'
     allowed_domains = ['herramientas.cl']
     start_urls = [
-        'https://herramientas.cl/subcategorias/101010/taladros-electricos']
+        'https://herramientas.cl/subcategorias/101010/taladros-electricos'
+        #'https://herramientas.cl/subcategorias/101010/taladros-electricos/0/0/0/0/0/12/pagina-2'
+    ]
 
     def parse(self, response):
         """Process Ferretek(herramientas.cl) products"""
@@ -18,14 +20,22 @@ class FerretekCrawlerSpider(scrapy.Spider):
             # define the XPaths
             XPATH_PRODUCT_NAME = ".//a[@class='nombreGrilla link']/text()"
             XPATH_PRODUCT_PRICE = ".//a[@class='valorGrilla link']/text()"
+            # define alternative Xpath if product is on sale
+            XPATH_PRODUCT_PRICE_SALE = ".//a[@class='valorGrilla link']//span[@class='conDescuento']/text()"
             XPATH_PRODUCT_BRAND = ".//a[@class='marca link']/span/text()"
             XPATH_PRODUCT_LINK = ".//a[@class='imgGrilla']/@href"
             XPATH_NEXT_PAGE = "//div[@class='cont100Centro paginador']//a[@class='paginate next']/@href"
 
             raw_product_name = product.xpath(XPATH_PRODUCT_NAME).extract()
             raw_product_price = product.xpath(XPATH_PRODUCT_PRICE).extract()
+            # check if regular price is not available
+            if raw_product_price:
+                pass
+            else:
+                raw_product_price = product.xpath(
+                    XPATH_PRODUCT_PRICE_SALE).extract()
             raw_product_brand = product.xpath(XPATH_PRODUCT_BRAND).extract()
-            link = product.xpath(XPATH_PRODUCT_LINK).extract_first()
+            link = product.xpath(XPATH_PRODUCT_LINK).get()
             raw_product_link = response.urljoin(link)
 
             # cleaning results
@@ -33,6 +43,7 @@ class FerretekCrawlerSpider(scrapy.Spider):
             ) if raw_product_name else None
             product_price = ''.join(raw_product_price).strip(
             ) if raw_product_price else None
+            # clean blank spaces, currency symbo $ and dot . from price
             product_price = re.sub(' |\$|\.', '', product_price)
             product_brand = ''.join(raw_product_brand).strip(
             ) if raw_product_brand else None
@@ -48,7 +59,7 @@ class FerretekCrawlerSpider(scrapy.Spider):
 
             # follow pagination link
             next_page_relative = product.xpath(
-                XPATH_NEXT_PAGE).extract_first()
+                XPATH_NEXT_PAGE).get()
             if next_page_relative:
                 next_page_url = response.urljoin(next_page_relative)
-                # yield scrapy.Request(url=next_page_url, callback=self.parse)
+                yield scrapy.Request(url=next_page_url, callback=self.parse)
