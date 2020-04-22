4fdc1a5bc5c774c8ae97b90675733f5f1873172c
Felipe Carrasco Villanueva <felipe.carrasco.villanueva@gmail.com>
2020-04-22 13:40:19 -0400

    refactor more code, add manufacturer

--- toolmania/toolmania/spiders/toolmania_crawler.py
+++ toolmania/toolmania/spiders/toolmania_crawler.py
@@ -7,20 +7,17 @@ class ToolmaniaCrawlerSpider(scrapy.Spider):
     """A Class Spider to scrape data from toolmania.cl"""
     name = 'toolmania_crawler'
     allowed_domains = ['toolmania.cl']
-    start_urls = [
-        'https://www.toolmania.cl/rotomartillos-252'
-        # url for whole category
-        #'https://www.toolmania.cl/herramientas-electricas-248'
-    ]
+    start_urls = ['https://www.toolmania.cl/pulidoras-562']
 
     def parse(self, response):
         """Process toolmania.cl products"""
-        # define product url xpath
-        XPATH_PRODUCT_LINK = "//a[@class='thumbnail product-thumbnail']/@href"
-        products = response.xpath(XPATH_PRODUCT_LINK).extract()
+        products = response.css('div.product-list')
+
         for product in products:
-            url = product
-            yield scrapy.Request(url, callback=self.parse_product)
+            # obtain product brand
+            brand = product.css('.product-manufacturer::text').get()
+            url = product.css('.thumbnail::attr(href)').get()
+            yield scrapy.Request(url, callback=self.parse_product, meta={'brand': brand})
 
         # follow pagination link
         XPATH_NEXT_PAGE = "//li[@class='page-item directional js-search-link']//a[@rel='next']/@href"
@@ -29,23 +26,32 @@ class ToolmaniaCrawlerSpider(scrapy.Spider):
             yield scrapy.Request(url=next_page, callback=self.parse)
 
     def parse_product(self, response):
-        # iterate over search results
+        """Get details from single product"""
         XPATH_SINGLE_PRODUCT = "//div[@class='single-product']"
+
         for product in response.xpath(XPATH_SINGLE_PRODUCT):
             # define xpaths for product details
             XPATH_PRODUCT_MODEL = ".//h5[@class='product-reference-single']/text()"
             XPATH_PRODUCT_NAME = ".//h1[@class='product-name-single mb-md-4']/text()"
             XPATH_PRODUCT_PRICE = ".//div[@class='product-prices margin__bottom__20']//span[@itemprop='price']/@content"
+            XPATH_PRODUCT_STOCK = ".//div[@class='form-group col-12']//p[@id='product-quantities']/text()"
 
+            # get and clean results
             product_model = product.xpath(XPATH_PRODUCT_MODEL).get()
-            # clean product model
             product_model = re.sub('Código de referencia: ', '', product_model)
-            # get current url
-            product_link = response.url
+            product_price = product.xpath(XPATH_PRODUCT_PRICE).get()
+            product_price = int(product_price)
+            product_stock = product.xpath(XPATH_PRODUCT_STOCK).get()
+            if product_stock:
+                product_stock = int(product_stock.split()[0])
+            else:
+                product_stock = 0
 
             yield {
+                'product_brand': response.meta['brand'],
                 'product_model': product_model,
+                'product_price': product_price,
                 'product_name': product.xpath(XPATH_PRODUCT_NAME).extract(),
-                'product_price': product.xpath(XPATH_PRODUCT_PRICE).extract(),
-                'product_link': product_link,
+                'product_stock': product_stock,
+                'product_link': response.url,
             }
