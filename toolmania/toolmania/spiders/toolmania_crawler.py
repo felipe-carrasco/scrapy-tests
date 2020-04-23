72dd1034892e2895cc709f3749bd566f8d00fd90
Felipe Carrasco Villanueva <felipe.carrasco.villanueva@gmail.com>
2020-04-23 14:02:04 -0400

    add product type for toolmania spider

--- toolmania/toolmania/spiders/toolmania_crawler.py
+++ toolmania/toolmania/spiders/toolmania_crawler.py
@@ -31,12 +31,14 @@ class ToolmaniaCrawlerSpider(scrapy.Spider):
 
         for product in response.xpath(XPATH_SINGLE_PRODUCT):
             # define xpaths for product details
+            XPATH_PRODUCT_TYPE = "//li[@itemprop='itemListElement'][5]//span/text()"
             XPATH_PRODUCT_MODEL = ".//h5[@class='product-reference-single']/text()"
             XPATH_PRODUCT_NAME = ".//h1[@class='product-name-single mb-md-4']/text()"
             XPATH_PRODUCT_PRICE = ".//div[@class='product-prices margin__bottom__20']//span[@itemprop='price']/@content"
             XPATH_PRODUCT_STOCK = ".//div[@class='form-group col-12']//p[@id='product-quantities']/text()"
 
             # get and clean results
+            product_type = product.xpath(XPATH_PRODUCT_TYPE).get()
             product_model = product.xpath(XPATH_PRODUCT_MODEL).get()
             product_model = re.sub('Código de referencia: ', '', product_model)
             product_price = product.xpath(XPATH_PRODUCT_PRICE).get()
@@ -48,10 +50,11 @@ class ToolmaniaCrawlerSpider(scrapy.Spider):
                 product_stock = 0
 
             yield {
-                'product_brand': response.meta['brand'],
+                'product_brand': response.meta['brand'].lower(),
                 'product_model': product_model,
+                'product_type': product_type,
                 'product_price': product_price,
-                'product_name': product.xpath(XPATH_PRODUCT_NAME).extract(),
+                'product_name': product.xpath(XPATH_PRODUCT_NAME).get(),
                 'product_stock': product_stock,
                 'product_link': response.url,
             }
